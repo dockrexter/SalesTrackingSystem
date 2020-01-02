9619b29441d8bfd51720d869c2cdb4e73a701ff0
Shaheer Rahi <mshaheer.rahi@gmail.com>
2020-01-02 10:44:37 +0500

    final

--- Employee/admin.py
+++ Employee/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Zones
 
 admin.site.register(Profile)
+admin.site.register(Zones)
 admin.site.site_header="Administration"
